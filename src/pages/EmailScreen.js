3e092b5fde96984c06ab7acb1ba04488ca8f427f
Svetlana210 <svetik.may24@gmail.com>
2023-05-30 19:27:49 +0200

    email screen

--- src/pages/EmailScreen.js
+++ src/pages/EmailScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const EmailScreen = ({navigation, route}) => {
   return (
     <View style={styles.master}>
-      <Text style={styles.text}>EmailScreen</Text>
+      <Text style={styles.text}>Enter your work email address</Text>
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => navigation.navigate('EmailScreen')}>
+        <Text style={styles.btnText}>CONTINUE WITH EMAIL</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -15,13 +20,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     // marginVertical: 30,
     // marginHorizontal: 30,
   },
   text: {
-    fontSize: 26,
-
-    marginBottom: 20,
+    fontSize: 32,
+    lineHeight: 38.4,
+    marginBottom: 24,
+    marginTop: 32,
+    // marginLeft: 16,
+    fontFamily: 'TTNorms-Medium',
   },
+  btn: {
+    backgroundColor: '#F0B528',
+    marginTop: 32,
+    paddingHorizontal: 79.5,
+    paddingVertical: 13,
+    borderRadius: 4,
+  },
+  btnText: {fontFamily: 'TTNorms-Bold', color: '#000000'},
 });
