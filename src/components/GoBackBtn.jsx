514814c2cb8157e025376ca920f9b7b2effb989d
Svetlana210 <svetik.may24@gmail.com>
2023-05-30 19:14:46 +0200

    backbtn

--- src/components/GoBackBtn.jsx
+++ src/components/GoBackBtn.jsx
@@ -3,20 +3,14 @@ import {TouchableOpacity} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
 
-// import {Feather} from '@expo/vector-icons';
-
 const GoBackBtn = () => {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity
       navigation={navigation}
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{width: 24, marginLeft: 16}}
-      // onPress={() => navigation.navigate('Posts')}
       onPress={() => navigation.goBack()}>
-      <AntDesign name="arrowleft" size={30} />
-      {/* <Feather name="arrow-left" size={22} color={'#212121cc'} /> */}
+      <AntDesign name="arrowleft" size={23} color={'#49454F'} />
     </TouchableOpacity>
   );
 };
