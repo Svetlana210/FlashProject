6fc4e07abd750a2df64f37479e701424b7b29e96
Svetlana210 <svetik.may24@gmail.com>
2023-06-05 09:49:05 +0200

    progress

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet, Image} from 'react-native';
 import AppText from '../components/AppText';
+import * as Progress from 'react-native-progress';
+// import {withTiming} from 'react-native-reanimated';
 
 let logo = require('../images/logoB.png');
 
-const SplashScreen = ({navigation, route}) => {
-  return (
-    <View style={styles.master}>
-      <Image source={logo} />
-      {/* <Text style={styles.title}>Welcome! </Text> */}
-      <AppText isMedium style={styles.title}>
-        Welcome!
-      </AppText>
-      <AppText style={styles.text}>Quick solution to answer surveys</AppText>
-      {/* <TouchableOpacity
-        style={styles.btn}
-        onPress={() => navigation.navigate('EmailScreen')}>
-        <AppText isBold style={styles.btnText}>
-          CONTINUE WITH EMAIL
+export default class SplashScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      progress: 0,
+      indeterminate: true,
+    };
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    let progress = 0;
+    this.setState({progress});
+    setTimeout(() => {
+      this.setState({indeterminate: true});
+      setInterval(() => {
+        progress += Math.random() / 5;
+        if (progress > 1) {
+          progress = 1;
+        }
+        this.setState({progress});
+      }, 500);
+    }, 1500);
+  }
+
+  render() {
+    return (
+      <View style={styles.master}>
+        <Image source={logo} />
+        {/* <Text style={styles.title}>Welcome! </Text> */}
+        <AppText isMedium style={styles.title}>
+          Welcome!
         </AppText>
-      </TouchableOpacity> */}
-    </View>
-  );
-};
-export default SplashScreen;
+        <AppText style={styles.text}>Quick solution to answer surveys</AppText>
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+          color="#FDF7E7"
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   master: {
@@ -50,4 +80,7 @@ const styles = StyleSheet.create({
     borderRadius: 4,
   },
   btnText: {color: '#000000', fontSize: 14},
+  progress: {
+    margin: 10,
+  },
 });
